db225a6b98f855266600256e5157b1f8867f3554
srmitu <68469263+srmitu@users.noreply.github.com>
2020-11-26 00:48:54 +0900

    マスク画像、輪郭及び重心の算出を別スレッド化
    digimono_cameraのクラスにて、マスク画像、切り取り画像(cutout)、輪郭、重心を抽出するスレッドを作成した。
    現時点ではすべての重心を抽出しないとmainが動かないようにはしてある

--- main.py
+++ main.py
@@ -21,17 +21,26 @@ print("start")
 #time.sleep(2)
 while True:
     #フレームを取得
-    frame = digimono_camera_frame.get_frame()
+    raw_frame = digimono_camera_frame.get_frame()
+    frame = raw_frame.copy()
     #生のカメラデータは早めに出力する
     digimono_camera_frame.show_frame()
+    digimono_camera.in_frame(raw_frame)
     #緑検出
     threshold = numpy.array([[85, 255, 230],[50, 45,35]])
-    mask,contours,point = digimono_camera.mask_detect(frame, threshold)
-    #print(point)
+    digimono_camera.in_threshold(threshold)
+    while True:
+        if(digimono_camera.get_task() == 1):
+            break
+    cutout = digimono_camera.get_cutout()
+    contours = digimono_camera.get_contours()
+    point = digimono_camera.get_point()
+    digimono_camera.in_task(0)
     cv2.rectangle(frame, (50,50), (150,150), (250,0,0), 3)
     cv2.rectangle(frame, (500,50), (600,150), (250,0,0), 3)
     for i in range(len(contours)):
         frame = cv2.drawContours(frame,contours,i,(0,150,0),3)
+        
         #start
         if(point[i][0]<150 and point[i][0]>50  and point[i][1]<150 and point[i][1]>50):
             cv2.circle(frame, (point[i][0],point[i][1]), 2, (0,0,250),20)
@@ -45,8 +54,10 @@ while True:
             if(cal_time==1):
                 cal_time=0
         else:
+        
             cv2.circle(frame, (point[i][0],point[i][1]), 2, (0,250,0),4)
-            
+        
+        
     if(cal_time==0 and cv2.waitKey(1) == 13):
         display_time=0
     if(display_time==1):
@@ -59,11 +70,8 @@ while True:
     cv2.namedWindow("FrameEdit", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("FrameEdit", 1000,800)
     cv2.imshow("FrameEdit",frame)
-    cv2.namedWindow("Mask", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("Mask", 1000,800)
-    cv2.imshow("Mask",mask)
+    cv2.namedWindow("Cutout", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Cutout", 1000,800)
+    cv2.imshow("Cutout",cutout)
 
     digimono_camera_frame.end_check()
-    #if cv2.waitKey(25) & 0xFF == ord('q'):
-    #    break
-
