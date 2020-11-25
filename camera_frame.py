c077fce7242f01a4fb414b38b0438320fd440e89
srmitu <r.mitu16@outlook.jp>
2020-11-25 12:16:08 +0900

    self処理の修正、スペルミスの修正
    self関係の処理を追加した。
    get_frameにてframeを得れるようにしている。
    checkのスペルミスがあったため修正した。

--- camera_frame.py
+++ camera_frame.py
@@ -6,10 +6,10 @@ class digimono_camera_frame(object):
     #ret: True時はカメラの使用許可    
     ret = 0
     #frame: 映像を保存. 初期化のため,0が入っている. 実際に使用するときは少し待ってから使用する
-    frame = 0
+    frame = cv2.imread('opencv.jpeg')
     #resize_vertical, resize_wide: 初期の画面の大きさを定義する
-    resize_vertical = 800
-    resize_wide = 1000
+    resize_vertical = 300
+    resize_wide = 400
     def __init__(self, camera_num):
         self.capture = cv2.VideoCapture(camera_num)
         self.thread = Thread(target = self.update, args=())
@@ -19,13 +19,11 @@ class digimono_camera_frame(object):
     def update(self):
         while self.capture.isOpened():
             self.ret, self.frame = self.capture.read()
-            digimono_camera_frame.ret = self.ret
-            digimono_camera_frame.frame = self.frame
             time.sleep(.01)
         exit(1)
 
     #毎フレーム読み出さなくてはならないメソッド
-    def end_cheack(self):
+    def end_check(self):
         if cv2.waitKey(1) == ord('q'):
             self.capture.release()
             cv2.destoryAllWindows()
@@ -33,5 +31,15 @@ class digimono_camera_frame(object):
 
     def show_frame(self):
         cv2.namedWindow("frame", cv2.WINDOW_NORMAL)
-        cv2.resizeWindow("frame", digimono_camera_frame.resize_wide, digimono_camera_frame.resize_vertical)
+        cv2.resizeWindow("frame", self.resize_wide, self.resize_vertical)
         cv2.imshow("frame", self.frame)
+
+    def get_frame(self):
+        return self.frame
+
+    def get_ret(self):
+        return self.ret
+
+    def import_resize(self, vertical, wide):
+        self.resize_wide = wide
+        self.resize_vertical = vertical
