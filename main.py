ed96e0f28aaad8dfb7262a3978448101af55bb38
srmitu <r.mitu16@outlook.jp>
2020-11-28 00:51:17 +0900

    青による入力も追加
    複数カラー入力を想定し、緑色の他に青色も追加した。
    緑の検出と青の検出はそれぞれ別のスレッドで動いている。

--- main.py
+++ main.py
@@ -4,16 +4,30 @@ import cv2
 import numpy
 import datetime
 import time
+import colorsys
 
 #実際の環境によって変更する変数
 camera_num = 0
 
-#緑検出
-threshold = numpy.array([[85, 255, 230],[50, 45,35]])
+#閾値
+threshold = []
+threshold.append(numpy.array([[85, 255, 230],[50, 45,35]]))
+threshold.append(numpy.array([[145, 180, 170],[95, 45,35]]))
 
+draw_color = []
+for i in range(2):
+    threshold_item = threshold[i]
+    h_ave = (threshold_item[0,0] + threshold_item[1,0]) / 2
+    s_ave = (threshold_item[0,1] + threshold_item[1,1]) / 2
+    v_ave = (threshold_item[0,2] + threshold_item[1,2]) / 2
+    r,g,b = colorsys.hsv_to_rgb(h_ave/180 , s_ave/255, v_ave/255)
+    draw_color.append([b*255, g*255, r*255])
+        
 #使用するクラス
 digimono_camera_frame = camera_frame.digimono_camera_frame(camera_num)
-digimono_camera = camera.digimono_camera(threshold)
+digimono_camera_list = []
+for num_list in range(2):
+    digimono_camera_list.append(camera.digimono_camera(threshold[num_list]))
 
 cal_time = 0
 display_time = 0
@@ -26,35 +40,42 @@ while True:
     #フレームを取得
     raw_frame = digimono_camera_frame.get_frame()
     frame = raw_frame.copy()
-    digimono_camera.put_frame(raw_frame)
-    
-    while True:
-        if(digimono_camera.get_task() == 1):
-            break
-    cutout = digimono_camera.get_cutout()
-    contours = digimono_camera.get_contours()
-    point = digimono_camera.get_point()
-    digimono_camera.put_task(0)
+    for num_list in digimono_camera_list:
+        num_list.put_frame(raw_frame)
+    #digimono_camera_list[1].put_frame(raw_frame)
+    for num_list in digimono_camera_list:
+        while True:
+            if(num_list.get_task() == 1):
+                break
+    cutout = []
+    contours = []
+    point = []
+    for num_list in digimono_camera_list:
+        cutout.append(num_list.get_cutout())
+        contours.append(num_list.get_contours())
+        point.append(num_list.get_point())
+        num_list.put_task(0)
     cv2.rectangle(frame, (50,50), (150,150), (250,0,0), 3)
     cv2.rectangle(frame, (500,50), (600,150), (250,0,0), 3)
-    for i in range(len(contours)):
-        frame = cv2.drawContours(frame,contours,i,(0,150,0),3)
+    for num_color in range(2):
+        for num_contours in range(len(contours[num_color])):
+            frame = cv2.drawContours(frame,contours[num_color],num_contours,(draw_color[num_color][0],draw_color[num_color][1],draw_color[num_color][2]),3)
         
-        #start
-        if(point[i][0]<150 and point[i][0]>50  and point[i][1]<150 and point[i][1]>50):
-            cv2.circle(frame, (point[i][0],point[i][1]), 2, (0,0,250),20)
-            if(cal_time==0):
-                cal_time=1
-                display_time=1
-                dt1=datetime.datetime.now()
-        #end
-        elif(point[i][0]<600 and point[i][0]>500  and point[i][1]<150 and point[i][1]>50):
-            cv2.circle(frame, (point[i][0],point[i][1]), 2, (0,0,250),20)
-            if(cal_time==1):
-                cal_time=0
-        else:
+            #start
+            if(point[num_color][num_contours][0]<150 and point[num_color][num_contours][0]>50  and point[num_color][num_contours][1]<150 and point[num_color][num_contours][1]>50):
+                cv2.circle(frame, (point[num_color][num_contours][0],point[num_color][num_contours][1]), 2, (0,0,250),20)
+                if(cal_time==0):
+                    cal_time=1
+                    display_time=1
+                    dt1=datetime.datetime.now()
+            #end
+            elif(point[num_color][num_contours][0]<600 and point[num_color][num_contours][0]>500  and point[num_color][num_contours][1]<150 and point[num_color][num_contours][1]>50):
+                cv2.circle(frame, (point[num_color][num_contours][0],point[num_color][num_contours][1]), 2, (0,0,250),20)
+                if(cal_time==1):
+                    cal_time=0
+            else:
         
-            cv2.circle(frame, (point[i][0],point[i][1]), 2, (0,250,0),4)
+                cv2.circle(frame, (point[num_color][num_contours][0],point[num_color][num_contours][1]), 2, (draw_color[num_color][0], draw_color[num_color][1], draw_color[num_color][2]),4)
         
         
     if(cal_time==0 and cv2.waitKey(1) == 13):
@@ -72,6 +93,9 @@ while True:
     cv2.imshow("FrameEdit",frame)
     cv2.namedWindow("Cutout", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("Cutout", 1000,800)
-    cv2.imshow("Cutout",cutout)
+    cv2.imshow("Cutout",cutout[0])
+    cv2.namedWindow("Cutout2", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Cutout2", 1000,800)
+    cv2.imshow("Cutout2",cutout[1])
 
     digimono_camera_frame.end_check()
