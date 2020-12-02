d84c6283a618d5b96192c95f07c756ef60d5637d
srmitu <68469263+srmitu@users.noreply.github.com>
2020-12-03 00:02:29 +0900

    Eventの撤廃、mask処理のマルチプロセス化成功
    event.wait()がうまく機能しないことが発覚したため、従来どおりtaskで管理することにした
    digimono_camera_mask内のmask_detectがマルチプロセス化に成功していることを確認した。

--- camera_mask.py
+++ camera_mask.py
@@ -1,77 +1,59 @@
 # -*- coding: utf-8 -*-
 import cv2
 import numpy
-from multiprocessing import Process, Manager, Value, Array
+from multiprocessing import Manager, Value, Array, Event
 class digimono_camera_mask(object):
 
-    def __init__(self, threshold, draw_color):
-        
+    def __init__(self, draw_color):
         #初期化
-        #共有設定
-        self.frame = Array('i', 0)
-        self.mask = Array('i', 0)
-        self.threshold = Array('i', 0)
-        self.draw_color = Array('i', 0)
-
-        self.threshold = threshold
         self.draw_color = tuple(draw_color)
-
-        self.resize_vertical = 600
-        self.resize_wide =720
-        self.permit_area = 500
 
     def __enter__(self):
         return self
     def __exit__(self, ex_type, ex_value, trace):
         pass
+
     #マスクを作成し、抽出した数とその中心点を求める
-    def mask_detect(self, frame, contours, point):
-        #i.value += 1
-        #hsv色空間に変換
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        #maskの作成(２値化)
-        if(len(self.threshold) == 2):
-            mask = cv2.inRange(hsv, self.threshold[1,], self.threshold[0,])
-        elif(len(self.threshold) == 4):
-            mask1 = cv2.inRange(hsv, self.threshold[1,], self.threshold[0,])
-            mask2 = cv2.inRange(hsv, self.threshold[3,], self.threshold[2,])
-            mask = mask1 + mask2
-        
-        if(len(self.threshold) == 2 or len(self.threshold) == 4):
-            self.mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, numpy.ones((5,5), numpy.uint8))
-            #輪郭の抽出
-            all_contours, hierarchy = cv2.findContours(self.mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            #必要な輪郭のみ抽出する
-            for num_all_contours in range(len(all_contours)):
-                if(cv2.contourArea(all_contours[num_all_contours]) > self.permit_area):
-                    contours.append(all_contours[num_all_contours])
-                    mu = cv2.moments(all_contours[num_all_contours])
-                    x,y = int(mu["m10"]/mu["m00"]), int(mu["m01"]/mu["m00"])
-                    point.append([x, y])
+    def mask_detect(self, hsv_frame, contours, point, threshold, permit_area, task):
+        #毎度更新必要:frame
+        #毎度更新される:contours, point, task
+        #値変更必要なし(初期に入れるのみ):threshold, point_erea
+        pattern = len(threshold)
+        task.value = 1
+        while True:
+            while task.value == 1:
+                pass
+            hsv = hsv_frame[0]
+            #maskの作成(２値化)
+            if(pattern == 2):
+                mask1 = cv2.inRange(hsv, threshold[1], threshold[0])
+                mask = mask1 
+            elif(pattern == 4):
+                mask1 = cv2.inRange(hsv, threshold[1], threshold[0])
+                mask2 = cv2.inRange(hsv, threshold[3], threshold[2])
+                mask = mask1 + mask2
+            if(pattern == 2 or pattern == 4):
+                #mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, numpy.ones((5,5), numpy.uint8))
+                #輪郭の抽出
+                all_contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                #必要な輪郭のみ抽出する
+                for num_all_contours in range(len(all_contours)):
+                    if(cv2.contourArea(all_contours[num_all_contours]) > permit_area):
+                        contours.append(all_contours[num_all_contours])
+                        mu = cv2.moments(all_contours[num_all_contours])
+                        x,y = int(mu["m10"]/mu["m00"]), int(mu["m01"]/mu["m00"])
+                        point.append([x, y])
+            
+            task.value = 1
 
-    def get_mask(self):
-        return self.mask
-    def get_cutout(self):
-        return self.cutout
-
-    def show_mask(self, num_screen):
-        title_mask = "Mask" + str(num_screen)
-        cv2.namedWindow(title_mask, cv2.WINDOW_NORMAL)
-        cv2.resizeWindow(title_mask, self.resize_wide, self.resize_vertical)
-        cv2.imshow(title_mask, self.mask)
-
-    def show_cutout(self, num_screen):
-        title_cutout = "Cutout" + str(num_screen)
-        cv2.namedWindow(title_cutout, cv2.WINDOW_NORMAL)
-        cv2.resizeWindow(title_cutout, self.resize_wide, self.resize_vertical)
-        cv2.imshow(title_cutout, self.cutout)
-
-    def draw_contours(self, edit_frame, contours):
+    def draw_contours(self, edit_frame, contours, thickness):
         return_edit_frame = edit_frame.copy()
         for num_contours in range(len(contours)):
-            return_edit_frame = cv2.drawContours(edit_frame, self.contours, num_contours, self.draw_color, 2)
+            return_edit_frame = cv2.drawContours(edit_frame, self.contours, num_contours, self.draw_color, thickness)
         return return_edit_frame
-
-    def put_resize(self, vertical, wide):
-        self.resize_wide = wide
-        self.resize_vertical = vertical
+    
+    def draw_point(self, frame, point):
+        return_frame = frame
+        for num_point in range(len(point)):
+            return_frame = cv2.circle(frame, tuple(point[num_point]), 5, tuple(self.draw_color), -1)
+        return return_frame
--- camera_position.py
+++ camera_position.py
@@ -3,90 +3,70 @@ import numpy as np
 from multiprocessing import Process, Value, Array
 
 class digimono_camera_position(object):
-    def __init__(self, no, mode, type_shape, shape, color, draw_color):
+    def __init__(self, draw_color, type_shape, shape):
         #初期化
-        #共有設定
-        self.no = Value('i', 0)
-        self.mode = Value('i', -1)
-        self.type_shape = Value('i', -1)
-        self.shape = Array('i', 0)
-        self.color = Value('i', -1)
-        self.draw_color = Array('i', 0)
-        self.cal_time_n = Value('i', 0)#0 = False, 1 = True
-        self.old_in_shape_n = Value('i', 0) #0 = False, 1 = True
-        
-        self.no.value = no
-        self.mode.value = mode
-        self.type_shape.value = type_shape
+        self.draw_color = tuple(draw_color) 
+        self.type_shape = type_shape
         self.shape = shape
-        self.color.value = color
-        self.draw_color = draw_color #tuple化必要
 
     def __enter__(self):
         pass
     def __exit__(self):
         pass
-        #print("exit")
 
-    def position_detect(self, rise_or_false, point, in_shape_position):
-        enter_time = 0
-        exit_time = 0
+    def position_detect(self, position, cal_time,  in_shape_position, task, state, mode, type_shape, shape):
+        #毎度更新必要:point, cal_time
+        #毎度更新される:in_shape_position, state, task
+        #値変更なし:mode, type_shape, shape 
         in_shape = 0 #0 = False, 1 = True
-        position = point[self.color.value]
-        if(position != []):
-            if(self.type_shape.value == 0):#四角
-                for num_position in range(len(position)):
-                    rectangle_x = abs(position[num_position][0]-self.shape[0][0])
-                    rectangle_y = abs(position[num_position][1]-self.shape[0][1])
-                    if(rectangle_x < self.shape[1][0] and rectangle_y  < self.shape[1][1]):
-                        in_shape = 1
-                        in_shape_position.append(position[num_position])
+        old_in_shape = 0
+        task.value = 1
+        while True:
+            while task.value == 1:
+                pass
+            task.value = 0
+            if(position != []):
+                if(type_shape == 0):#四角
+                    for num in range(len(position)):
+                        rectangle_x = abs(position[num][0]-shape[0][0])
+                        rectangle_y = abs(position[num][1]-shape[0][1])
+                        if(rectangle_x < shape[1][0] and rectangle_y  < shape[1][1]):
+                            in_shape = 1
+                            in_shape_position.append(position[num])
 
-            elif(self.type_shape.value == 1):#楕円
-                for num_position in range(len(position)):
-                    ellipse_x = ((position[num_position][0]-self.shape[0][0]) / self.shape[1][0]) ** 2 
-                    ellipse_y = ((position[num_position][1]-self.shape[0][1]) / self.shape[1][1]) ** 2
-                    ellipse = ellipse_x + ellipse_y
-                    if(ellipse < 1):
-                        in_shape = 1
-                        in_shape_position.append(position[num_position])
+                elif(type_shape == 1):#楕円
+                    for num in range(len(position)):
+                        ellipse_x = ((position[num][0]-shape[0][0]) / shape[1][0]) ** 2 
+                        ellipse_y = ((position[num][1]-shape[0][1]) / shape[1][1]) ** 2
+                        ellipse = ellipse_x + ellipse_y
+                        if(ellipse < 1):
+                            in_shape = 1
+                            in_shape_position.append(position[num])
 
-            if((self.old_in_shape_n.value == 0 and in_shape == 1) and ((self.mode.value <= 1 and self.cal_time_n.value == 0) or self.mode.value > 1)):
-                rise_or_false[self.no.value] = str("rise")
-            elif((self.old_in_shape_n.value == 1 and in_shape == 0) and ((self.mode <= 1 and self.cal_time.value == 0) or self.mode.value > 1)):
-                rise_or_false[self.no.value] = str("fall")
-            elif(in_shape == 1):
-                rise_or_false[self.no.value] = str("in")
-            elif(in_shape == 0):
-                rise_or_false[self.no.value] = str("out")
-            else:
-                rise_or_false[self.no.value] = str("none")
-
-    def get_cal_time(self):
-        return_cal_time = False
-        if(self.cal_time_n.value == 1):
-            return_cal_time = True
-        return return_cal_time
-    def put_cal_time(self, bool_cal_time):
-        if(bool_cal_time == False):
-            self.cal_time_n.value = 0
-        elif(bool_cal_time == True):
-            self.cal_time_n.value = 1
-
+                if((old_in_shape == 0 and in_shape == 1) and ((mode <= 1 and cal_time == 0) or mode > 1)):
+                    state.value = 10 #rise
+                elif((old_in_shape == 1 and in_shape == 0) and ((mode <= 1 and cal_time == 0) or mode > 1)):
+                    state.value = 11 #fall
+                elif(in_shape == 1):
+                    state.value = 1 #in
+                elif(in_shape == 0):
+                    state.value = 0 #out
+                else:
+                    state.value = 20 #none
+            task.value = 1
+    
     def draw_in_shape_position(self, frame, in_shape_position):
         return_frame = frame
         for num_position in range(len(in_shape_position)):
-            return_frame = cv2.circle(frame, tuple(in_shape_position[num_point]), 20, tuple(self.draw_color), -1)
+            return_frame = cv2.circle(frame, tuple(in_shape_position[num_position]), 20, tuple(self.draw_color), -1)
         return return_frame
 
     def draw_shape(self, frame):
         return_frame = frame
-        print(3)
         if(self.type_shape == 0):
             left_up = ((self.shape[0][0]-self.shape[1][0]), (self.shape[0][1]+self.shape[1][1]))
             right_down = ((self.shape[0][0]+self.shape[1][0]), (self.shape[0][1]-self.shape[1][1]))
             return_frame = cv2.rectangle(frame, left_up, right_down, tuple(self.draw_color), 3)
-            print(4)
         elif(self.type_shape == 1):
             return_frame = cv2.ellipse(frame, (tuple(self.shape[0]),tuple(self.shape[1] * 2),0), tuple(self.draw_color), 3)
         return return_frame
