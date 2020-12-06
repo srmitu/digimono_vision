0bbd1bbf29c9f6cf2c468352ace0ef62c169d2a3
srmitu <r.mitu16@outlook.jp>
2020-12-06 17:30:03 +0900

    コマンドラインに出力する文字を変更

--- camera_capture.py
+++ camera_capture.py
@@ -21,20 +21,19 @@ class digimono_camera_capture(object):
         dt_start = datetime.datetime.now()
         video_path = 'result/' + str(datetime.datetime.today().date()) 
         video_name = video_path + '/' + str(dt_start.strftime('%H:%M:%S')) + '.mp4'
-        print(video_path)
         if not os.path.isdir(video_path):
             os.makedirs(video_path)
-        #video_pass = 'result/test.mp4' 
         writer = cv2.VideoWriter(video_name, video_format, fps/10, (wide, height))
         print("record start")
         ret.value = True
         while(ret.value == True):
-            print('\r' + "record", end="")
+            print('\r' + "recording in " + str(video_name), end="")
             while(task.value == False):
                 time.sleep(0.03)
             dt_now = datetime.datetime.now()
             dt = dt_now - dt_start
             if(dt.seconds >= 30):
+                print("...finish")
                 writer.release()
                 dt_start = dt_now
                 video_path = 'result/' + str(datetime.datetime.today().date()) 
@@ -42,13 +41,12 @@ class digimono_camera_capture(object):
                 if not os.path.isdir(video_path):
                     os.makedirs(video_path)
                 writer = cv2.VideoWriter(video_name, video_format, fps/10, (wide, height))
-                print("change")
             writer.write(frame[0])
             if(ret.value == False):
                 break
             task.value = False
         writer.release()
-        print("record_end")
+        print('\n' + "record_end")
 
     def put_frame(self, in_frame):
         self.frame.append(in_frame)
