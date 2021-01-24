034008cda5bb0581fe808540b02b37b38534d3bd
srmitu <68469263+srmitu@users.noreply.github.com>
2021-01-24 15:31:02 +0900

    仮のmain.pyの削除

--- main.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import camera_main
-#camera_mainの定義,コンフィグファイルの読み込み,無限ループに入る前に済ませておく処理をすべて行う
-digi_main = camera_main.digimono_camera_main()
-
-#無限ループ
-while(digi_main.get_ret() == True):
-    digi_main.get_frame()
-digi_main.main_end()
